b5bba1af04803e48cacc5e93e7b2f17374237f8e
kbrady-rgb <58741813+kbrady-rgb@users.noreply.github.com>
2020-09-15 16:03:35 -0500

    Completed project

---
+++ StudentStructureBrady/StudentStructureBrady.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace StudentStructureBrady
+{
+    partial class studentStructureForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCreateStudent = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.txtMajor = new System.Windows.Forms.TextBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblID = new System.Windows.Forms.Label();
+            this.lblMajor = new System.Windows.Forms.Label();
+            this.lblNameOutput = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCreateStudent
+            // 
+            this.btnCreateStudent.Location = new System.Drawing.Point(45, 276);
+            this.btnCreateStudent.Name = "btnCreateStudent";
+            this.btnCreateStudent.Size = new System.Drawing.Size(159, 34);
+            this.btnCreateStudent.TabIndex = 0;
+            this.btnCreateStudent.Text = "&Create Student";
+            this.btnCreateStudent.UseVisualStyleBackColor = true;
+            this.btnCreateStudent.Click += new System.EventHandler(this.BtnCreateStudent_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Location = new System.Drawing.Point(265, 273);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(138, 37);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Text = "&Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.BtnExit_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(116, 37);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(255, 22);
+            this.txtName.TabIndex = 2;
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(117, 93);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(254, 22);
+            this.txtID.TabIndex = 3;
+            // 
+            // txtMajor
+            // 
+            this.txtMajor.Location = new System.Drawing.Point(116, 147);
+            this.txtMajor.Name = "txtMajor";
+            this.txtMajor.Size = new System.Drawing.Size(255, 22);
+            this.txtMajor.TabIndex = 4;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(53, 37);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(45, 17);
+            this.lblName.TabIndex = 5;
+            this.lblName.Text = "Name";
+            // 
+            // lblID
+            // 
+            this.lblID.AutoSize = true;
+            this.lblID.Location = new System.Drawing.Point(53, 93);
+            this.lblID.Name = "lblID";
+            this.lblID.Size = new System.Drawing.Size(21, 17);
+            this.lblID.TabIndex = 6;
+            this.lblID.Text = "ID";
+            // 
+            // lblMajor
+            // 
+            this.lblMajor.AutoSize = true;
+            this.lblMajor.Location = new System.Drawing.Point(53, 150);
+            this.lblMajor.Name = "lblMajor";
+            this.lblMajor.Size = new System.Drawing.Size(43, 17);
+            this.lblMajor.TabIndex = 7;
+            this.lblMajor.Text = "Major";
+            // 
+            // lblNameOutput
+            // 
+            this.lblNameOutput.AutoSize = true;
+            this.lblNameOutput.Location = new System.Drawing.Point(53, 221);
+            this.lblNameOutput.Name = "lblNameOutput";
+            this.lblNameOutput.Size = new System.Drawing.Size(55, 17);
+            this.lblNameOutput.TabIndex = 8;
+            this.lblNameOutput.Text = "Output:";
+            // 
+            // studentStructureForm
+            // 
+            this.AcceptButton = this.btnCreateStudent;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(457, 365);
+            this.Controls.Add(this.lblNameOutput);
+            this.Controls.Add(this.lblMajor);
+            this.Controls.Add(this.lblID);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.txtMajor);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnCreateStudent);
+            this.Name = "studentStructureForm";
+            this.Text = "Student Structure";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCreateStudent;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.TextBox txtMajor;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblID;
+        private System.Windows.Forms.Label lblMajor;
+        private System.Windows.Forms.Label lblNameOutput;
+    }
+}
+
---
+++ StudentStructureBrady/StudentStructureBrady.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/***************************************************************
+* Name        : Student Structure
+* Author      : Kabrina Brady
+* Created     : 10/13/2019
+* Course      : CIS 169 - C#
+* Version     : 1.0
+* OS          : Windows 10
+* Copyright   : This is my own original work
+* Description : Program accepts user input and creates a new student with the input provided
+*               Input:  Name, ID, major, button clicks
+*               Output: Name, ID, major
+* Academic Honesty: I attest that this is my original work.
+* I have not used unauthorized source code, either modified or unmodified. I have not given other fellow student(s) access to my program.         
+***************************************************************/
+
+namespace StudentStructureBrady
+{
+    //Student structure
+    struct Student
+    {
+        public string name;
+        public int studentID;
+        public string major;
+    }
+
+    public partial class studentStructureForm : Form
+    {
+        public studentStructureForm()
+        {
+            InitializeComponent();
+        }
+
+        //creates method that assigns user input to structure elements
+        private void DisplayStudent(ref Student studentInfo)
+        {
+            try
+            {
+                studentInfo.name = txtName.Text;
+                studentInfo.studentID = int.Parse(txtID.Text);
+                studentInfo.major = txtMajor.Text;
+            }
+            catch
+            {
+                MessageBox.Show("Invalid input."); //error message if studentID isn't a number
+            }
+        }
+
+    private void BtnCreateStudent_Click(object sender, EventArgs e)
+        {
+            //create instance of StudentInfo structure
+            Student details = new Student();
+
+            //get user input
+            DisplayStudent(ref details);
+
+            try
+            {
+                //show info
+                lblNameOutput.Text = details.name.ToString() + ", " + details.studentID.ToString() + ", " + details.major.ToString();
+            }
+            catch
+            {
+                //error message if no input
+                MessageBox.Show("Please enter student details.");
+            }
+        }
+
+        private void BtnExit_Click(object sender, EventArgs e)
+        {
+            this.Close(); //closes form
+        }
+    }
+}
